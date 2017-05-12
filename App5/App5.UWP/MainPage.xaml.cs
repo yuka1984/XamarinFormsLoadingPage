10e3edd8495032c97116971453077567e83c0c6b
yuka1984 <newtamafuyou@gmail.com>
2017-05-13 01:27:17 +0900

    Revert "Revert "Revert "Cleanup Code"""
    This reverts commit c2ec555f53b23d2145d5669a1fa7192ee6cad900.

--- App5/App5.UWP/MainPage.xaml.cs
+++ App5/App5.UWP/MainPage.xaml.cs
@@ -1,10 +1,25 @@
-﻿namespace App5.UWP
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace App5.UWP
 {
     public sealed partial class MainPage
     {
         public MainPage()
         {
-            InitializeComponent();
+            this.InitializeComponent();
 
             LoadApplication(new App5.App());
         }
