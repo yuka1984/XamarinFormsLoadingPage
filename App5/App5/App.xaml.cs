c2ec555f53b23d2145d5669a1fa7192ee6cad900
yuka1984 <newtamafuyou@gmail.com>
2017-05-13 01:26:31 +0900

    Revert "Revert "Cleanup Code""
    This reverts commit 0e93862d65d08c55964681b8a0acc9bb3f366fb0.

--- App5/App5/App.xaml.cs
+++ App5/App5/App.xaml.cs
@@ -1,36 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace App5
 {
-	public partial class App : Application
-	{
-		public App ()
-		{
-			InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
             var tabbed = new TabbedPage();
-            tabbed.Children.Add(new MainPage(){Title = "page1"});
-            tabbed.Children.Add(new MainPage(){Title = "page2"});
-			MainPage = tabbed;
-		}
+            tabbed.Children.Add(new MainPage {Title = "page1"});
+            tabbed.Children.Add(new MainPage {Title = "page2"});
+            MainPage = tabbed;
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
