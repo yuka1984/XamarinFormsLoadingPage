0e93862d65d08c55964681b8a0acc9bb3f366fb0
yuka1984 <newtamafuyou@gmail.com>
2017-05-13 01:26:11 +0900

    Revert "Cleanup Code"
    This reverts commit 1e2e3aa680533028d0051e1ba0b8012dd408cd84.

--- App5/App5/MainPage.xaml.cs
+++ App5/App5/MainPage.xaml.cs
@@ -1,19 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace App5
 {
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
             Button.Clicked += ButtonOnClicked;
-        }
+		}
 
-        private void ButtonOnClicked(object sender, EventArgs eventArgs)
-        {
-            IsBusy = !IsBusy;
-        }
-    }
+	    private void ButtonOnClicked(object sender, EventArgs eventArgs)
+	    {
+	        IsBusy = !IsBusy;
+	    }
+	}
 }
